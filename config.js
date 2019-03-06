7a2df65567affa90650971d976cc7b7ef312e6db
Skipprod <42744289+Skipprod@users.noreply.github.com>
2019-03-06 10:16:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const TOKEN = "5b49f8e544a3eff411de0982aa67749588367a9aedb6904134dfa7ee3ceb34bbd7e9ed6b500a1c0d21775";
+const GROUP_ID = 177938315;
+const BTN_TEXT = ["govno", "zalupa", "raid", "reidbyartem"];
+const TIME = 100;
+const CHAT_SPAM = `
+[id355272959|АРТЕМ] лучший кодер
+`;
+const HELLO_TEXT = `Лучший игровой бот @`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
